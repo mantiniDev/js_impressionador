af63f5115ea67a048411e6ec58859816456893c5
mantiniDev <pmantinidev@gmail.com>
2023-06-28 00:37:29 -0300

    atualizando

--- Modulo3/funcao.js
+++ Modulo3/funcao.js
@@ -4,7 +4,12 @@ function pegarComanda() {
     console.log('Bom apetite!')
     console.log('---------------------')
 }
-
+function pegarComanda() {
+    console.log('olá, boa noite !!')
+    console.log('Pegue aqui sua comanda!')
+    console.log('Bom apetite!')
+    console.log('---------------------')
+}
 pegarComanda();
 pegarComanda();
 
